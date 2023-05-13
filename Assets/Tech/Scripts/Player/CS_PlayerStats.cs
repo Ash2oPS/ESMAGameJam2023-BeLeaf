2d409af14341bed005ecbf5f651d7211a6f58c45
RENNES\s.rey <rey.babou35@free.fr>
2023-05-13 20:47:04 +0200

    Jauge fonctionnelle, Sébastien

--- Assets/Tech/Scripts/Player/CS_PlayerStats.cs
+++ Assets/Tech/Scripts/Player/CS_PlayerStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,27 @@ using UnityEngine;
 public class CS_PlayerStats : MonoBehaviour
 {
     [SerializeField] private float _playerHP;
+    [SerializeField] private CS_HealthBar _healthBar;
+    [SerializeField] private float _coefficient;
     public float PlayerHP => _playerHP;
+
+    private void Update()
+    {
+        DecreaseHPOverTime();
+    }
 
     public void SetHP(float _newHP)
     {
         _playerHP = _newHP;
-
+        _healthBar.SetJauge(_newHP);
         if (_playerHP == 0)
         {
             Debug.Log("Joueur Dead");
         }
-        
+    }
+    
+    private void DecreaseHPOverTime()
+    {
+        SetHP(_playerHP - _coefficient * Time.deltaTime);
     }
 }
---
+++ Assets/Tech/Scripts/UI/CS_HealthBar.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CS_HealthBar : MonoBehaviour
+{
+    [Header("---References---")]
+    [SerializeField] private Slider _slider;
+
+    [Header("---Data---")]
+    [SerializeField] private float _coeffJauge;
+  
+    //private 
+    
+    //getters
+    public Slider Slider => _slider;
+
+    public void SetMaxHealth(float health)
+    {
+        _slider.maxValue = health;
+        _slider.value = health;
+    }
+    
+    public void SetJauge(float _health)
+    {
+        _slider.value = _health;
+    }
+    
+}
